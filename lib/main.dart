3233c52d8ef221cdeaa9c737e23ed931e3f8a4ef
mayorbyrne <kmoritz@gmail.com>
2023-08-16 20:16:55 -0500

    feat: add a drawer

--- lib/main.dart
+++ lib/main.dart
@@ -10,32 +10,92 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: MyHomePage(title: "Kevin's Site"),
       color: Colors.white,
-      title: "Kevin Personal Site",
+      title: "Kevin's Site",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
-      home: Scaffold(
-        backgroundColor: Colors.grey,
-          appBar: AppBar(
-              leading: IconButton(
-                  icon: Icon(Icons.menu),
-                  onPressed: () => print("hello world")),
-              title: Text("KEVIN MORITZ")),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 16),
-                  child: Image.asset("assets/images/Kirk_Van_Houten.png"),
-                ),
-                TextButton(
-                    child: Text("Hi. I'm Kevin."),
-                    onPressed: () => print("hello world")),
-              ],
-            ),
-          )),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _selectedIndex = 0;
+
+  var _widgetOptions = [
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/images/Kirk_Van_Houten.png"),
+      ],
+    ),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [Text("HELLO WORLD")],
+    ),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [Text("kmoritz@gmail.com")],
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(title: Text("KEVIN MORITZ")),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                ),
+                child: Text("Kevin Moritz"),
+              ),
+              ListTile(
+                  title: Text("About"),
+                  onTap: () {
+                    _onItemTapped(0);
+                    Navigator.pop(context);
+                  }),
+              ListTile(
+                  title: Text("Projects"),
+                  onTap: () {
+                    // Update the state of the app
+                    _onItemTapped(1);
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  }),
+              ListTile(
+                title: Text("Contact"),
+                onTap: () {
+                  // Update the state of the app
+                  _onItemTapped(2);
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: Center(child: _widgetOptions[_selectedIndex]));
   }
 }
